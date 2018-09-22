96ae4365103c9fc2ea9b6e6d129d5c4f0d88ae8c
Moacir Braga <contato@moacirbrg.org>
2018-09-22 12:57:38 -0700

    Fix license template

--- woocommerce-enviofacil.php
+++ woocommerce-enviofacil.php
@@ -8,24 +8,25 @@
  * License:              GPLv3 or later
  * Text Domain:          wc-enviofacil
  * Domain Path:          /languages
- * WC requires at least: 3.0.0
+ * WC requires at least: 4.9.0
  * WC tested up to:      4.9.8
  *
- * WooCommerce Correios is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * any later version.
+ * Copyright (C) 2018  Moacir Braga
  *
- * WooCommerce Correios is distributed in the hope that it will be useful,
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with WooCommerce Correios. If not, see
- * <https://www.gnu.org/licenses/gpl-2.0.txt>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/gpl-3.0.txt>.
  *
- * @package WooCommerce_Correios
+ * @package WooCommerce_EnvioFacil
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
