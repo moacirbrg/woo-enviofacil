23f13617accc1089cec7b2b964bc7752920f5e38
Moacir Braga <contato@moacirbrg.org>
2018-10-17 21:46:41 -0300

    Version update

--- woo-enviofacil.php
+++ woo-enviofacil.php
@@ -4,7 +4,7 @@
  * Plugin URI:           https://github.com/moacirbrg/woo-enviofacil
  * Description:          Adds Envio Fácil shipping methods to WooCommerce
  * Author:               Moacir Braga
- * Version:              0.1.1
+ * Version:              0.1.2
  * License:              GPLv2 or later
  * Text Domain:          woo-enviofacil
  * Domain Path:          /languages
